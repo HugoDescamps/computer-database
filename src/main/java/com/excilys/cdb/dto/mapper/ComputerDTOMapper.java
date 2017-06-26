ffdb3d05a35d536fdf8ef04ee7c764d15bf0ff21
=Hugo Descamps <=hdescamps@excilys.com>
2017-06-26 17:05:47 +0200

    Debugging mapping servlet, implémentation servlet pagination, ajout DTO

---
+++ src/main/java/com/excilys/cdb/dto/mapper/ComputerDTOMapper.java
@@ -0,0 +1,47 @@
+package com.excilys.cdb.dto.mapper;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.excilys.cdb.dto.ComputerDTO;
+import com.excilys.cdb.model.Company;
+import com.excilys.cdb.model.Computer;
+
+public class ComputerDTOMapper {
+
+	private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+	public static ComputerDTO createDTO(Computer computer) {
+
+		ComputerDTO computerDTO = new ComputerDTO();
+
+		computerDTO.setId(computer.getId());
+		computerDTO.setName(computer.getName());
+
+		if (computer.getIntroduced() != null) {
+
+			computerDTO.setIntroduced(computer.getIntroduced().format(formatter));
+		}
+		if (computer.getDiscontinued() != null) {
+			computerDTO.setDiscontinued(computer.getDiscontinued().format(formatter));
+		}
+		if (computer.getCompany() != null) {
+			computerDTO.setCompany(CompanyDTOMapper.createDTO(new Company(computer.getCompany().getId(), computer.getCompany().getName())));
+			;
+		}
+
+		return computerDTO;
+	}
+
+	public static List<ComputerDTO> createDTO(List<Computer> computers) {
+		List<ComputerDTO> computerDTOList = new ArrayList<ComputerDTO>();
+
+		for (Computer computer : computers) {
+			computerDTOList.add(createDTO(computer));
+		}
+		return computerDTOList;
+
+	}
+
+}
