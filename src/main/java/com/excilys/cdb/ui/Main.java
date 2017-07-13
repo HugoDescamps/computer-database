403edebd8abff8b067751a1329aa181fe988ada9
=Hugo Descamps <=hdescamps@excilys.com>
2017-07-13 17:01:04 +0200

    [Spring] Spring logging w/ slf4j

--- src/main/java/com/excilys/cdb/ui/Main.java
+++ src/main/java/com/excilys/cdb/ui/Main.java
@@ -1,7 +1,5 @@
 package com.excilys.cdb.ui;
 
-import java.util.logging.Logger;
-
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.excilys.cdb.config.CLIConfig;
@@ -11,16 +9,13 @@ import com.excilys.cdb.service.ComputerService;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		final Logger log = Logger.getLogger(Main.class.getName());
 
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-		log.warning("c'est parti");
 		context.register(CLIConfig.class);
 		context.refresh();
 
 		Commands commands = new Commands();
-		
+
 		commands.setCompanyService(context.getBean(CompanyService.class));
 		commands.setComputerService(context.getBean(ComputerService.class));
 
